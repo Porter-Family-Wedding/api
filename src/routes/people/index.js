95deff10b5b07ee3bdc077091ea0135ed7aef972
Kalob Porter <kalob@kalob.net>
2020-10-31 17:54:36 -0400

    Working people integration

--- src/routes/people/index.js
+++ src/routes/people/index.js
@@ -3,10 +3,14 @@ import { Router } from 'restify-router';
 import auth from 'src/middleware/auth';
 
 import me from './me';
+import create from './create';
+import update from './update';
 
 const router = new Router();
 
 
 router.get('/people/me', auth, me);
+router.post('/people', auth, create);
+router.put('/people/:id', auth, update);
 
 export default router;

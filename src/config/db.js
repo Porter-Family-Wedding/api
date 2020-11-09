3768db5b5ad6623d48a9b6efa1823ea6963df5f1
Kalob Porter <kalob@kalob.net>
2020-11-09 09:14:03 -0500

    Production deploy

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,11 @@ import * as pg from 'pg';
 
 import * as models from 'src/models';
 
-const user = process.env.APP_DB_USER || 'postgres';
-const host = process.env.APP_DB_HOST || 'db';
-const port = process.env.APP_DB_PORT || 5432;
-const name = process.env.APP_DB_NAME || 'postgres';
-const password = process.env.APP_DB_PASSWORD ? `:${process.env.APP_DB_PASSWORD}` : '';
+const user = process.env.DB_USER || 'postgres';
+const host = process.env.DB_HOST || 'db';
+const port = process.env.DB_PORT || 5432;
+const name = process.env.DB_NAME || 'postgres';
+const password = process.env.DB_PASSWORD ? `:${process.env.DB_PASSWORD}` : '';
 
 export const dbUrl = `postgres://${user}${password}@${host}:${port}/${name}`;
 
